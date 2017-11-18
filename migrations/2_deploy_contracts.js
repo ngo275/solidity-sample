e857bf0d72e753c6babfe85acab8465510c01afc
ngo275 <ngo38114@gmail.com>
2017-11-18 18:31:32 +0900

    added kovan settings

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,15 @@
-var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
-var ProofOfExistence2 = artifacts.require("./ProofOfExistence2.sol");
+// var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
+// var ProofOfExistence2 = artifacts.require("./ProofOfExistence2.sol");
 var ProofOfExistence3 = artifacts.require("./ProofOfExistence3.sol");
-var Purchase = artifacts.require("./Purchase.sol");
+// var Purchase = artifacts.require("./Purchase.sol");
 var Mortal = artifacts.require("./Mortal.sol");
 var Greet = artifacts.require("./Greet.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(ProofOfExistence1);
-  deployer.deploy(ProofOfExistence2);
+  // deployer.deploy(ProofOfExistence1);
+  // deployer.deploy(ProofOfExistence2);
   deployer.deploy(ProofOfExistence3);
-  deployer.deploy(Purchase);
+  // deployer.deploy(Purchase);
   deployer.deploy(Mortal);
   deployer.deploy(Greet, accounts[0]);
 };
